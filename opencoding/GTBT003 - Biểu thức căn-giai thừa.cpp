051263c1e8b6e7c9372e8d956cfd3b619140b81f
chienthan2vn <91756849+chienthan2vn@users.noreply.github.com>
2022-04-13 06:06:17 +0700

    Update GTBT003 - Biểu thức căn-giai thừa.cpp

--- opencoding/GTBT003 - Biểu thức căn-giai thừa.cpp
+++ opencoding/GTBT003 - Biểu thức căn-giai thừa.cpp
@@ -1,33 +1,24 @@
-//GTBT003 - Biểu thức căn-giai thừa
-#include <iostream>
-#include <math.h>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
-int giaithua(int a)
-{
-    if(a == 1 || a == 0)
-        return 1;
-    return a*giaithua(a-1);
-}
-float can(int n)
-{
-    float result = 0; // result lưu giá trị của result cũ sau đó + với giai thừa hiện tại và lấy căn
-    for(int i = 1; i <= n; i ++)
-    {
-        result = pow(giaithua(i)+result,1.0/(i+1));    // += ra sum
-    }
-    //cout << setprecision(3) << fixed << result << endl;
-    return result;
-    
-}
-int main()
-{
-    int test,n;
-    cin >> test;
-    while(test --)
-    {
-        cin >> n;
-        cout << setprecision(3) << fixed << can(n) << " \n";
+long long giaiThua(int n);
+int main() {
+    int n,test;
+    cin>>test;
+    while (test--){
+        cin>>n;
+        float sum=0,k;
+        for (int i = 1; i <=n ; i++) {
+            k = 1.0/(i+1);
+            sum= (float ) pow((giaiThua(i)+sum),k);
+
+        }
+        cout<<setprecision(3)<<fixed<<sum<<endl;
     }
     return 0;
+}
+long long giaiThua(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * giaiThua(n - 1);
 }
